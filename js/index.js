df0bbfc78e8d963b5a9175fda12727ce37c317cc
David Tran <David Tran>
2016-10-26 11:18:05 -0400

    More mobile friendly interface, layed out a basic example skeleton of how the work section will probably work.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * Created by David on 9/20/2016.
  */
 
-var jumboArray = ["Aspiring Computer Engineer.", "Board Game Enthusiast.", "Rookie Crocheter.", "Energetic Dancer.", "Lifelong Learner."];
+var jumboArray = ["Aspiring Computer Engineer.", "Board Game Enthusiast.", "Rookie Crocheter.", "Energetic Dancer.", "Lifelong Learner.", "Indie Game Developer"];
 var arrayIndex = 0;
 var main = setInterval(updateJumbo, 4000);
 
@@ -40,6 +40,22 @@ $(".educLink").click(function () {
         $(".body-content").load("content/educ.html", $(".body-content").fadeIn());
     });
 });
+
+//For mobile, when you click the menu, you should scroll to top to get the dropdown list.
+$(".scrollTop").on('click', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: 0
+    }, 300);
+})
+
+$(".mobNav").on("click", function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $(".body-content").offset().top
+    }, 300);
+})
 
 $(document).ready(function () {
     $(".body-content").load("content/home.html");
